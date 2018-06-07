76d1c4f615d380b52c6a3e5d5a69c65dca2a9f39
bwu <583479389@qq.com>
2018-06-07 23:54:53 +0800

    项目框架初始化

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # furniture-image-retrieval
+
+furniture-common
+
+
+funiture-domain
+Mariadb
+BeetlSQL
+ElasticSearch
+LIRE
+
+furniture-api
+
+
+furniture-service
+SpringBoot
+
+
+furniture-web
+SpringBoot
+Beetl
+
+
---
+++ src/main/java/org/calculous/furniture/FurnitureImageRetrievalApplication.java
@@ -0,0 +1,12 @@
+package org.calculous.furniture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureImageRetrievalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureImageRetrievalApplication.class, args);
+	}
+}
