bf76e856ce7875b4efae190b283c62e05ad3aced
bwu <583479389@qq.com>
2018-06-15 14:00:22 +0800

    springboot集成beetl

---
+++ furniture-service/src/main/java/org/furniture/service/config/BeetlSqlConfig.java
@@ -0,0 +1,65 @@
+package org.furniture.service.config;
+
+import javax.sql.DataSource;
+
+import org.beetl.sql.core.ClasspathLoader;
+import org.beetl.sql.core.Interceptor;
+import org.beetl.sql.core.UnderlinedNameConversion;
+import org.beetl.sql.core.db.MySqlStyle;
+import org.beetl.sql.ext.DebugInterceptor;
+import org.beetl.sql.ext.spring4.BeetlSqlDataSource;
+import org.beetl.sql.ext.spring4.BeetlSqlScannerConfigurer;
+import org.beetl.sql.ext.spring4.SqlManagerFactoryBean;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+/**
+ * @Description ${DESCRIPTION}
+ * @Author bwu
+ * @Create 10/06/2018 23:56.
+ */
+@Configuration
+public class BeetlSqlConfig {
+    @Bean(name = "beetlSqlScannerConfigurer")
+    public BeetlSqlScannerConfigurer getBeetlSqlScannerConfigurer() {
+        BeetlSqlScannerConfigurer conf = new BeetlSqlScannerConfigurer();
+        conf.setBasePackage("boottest");
+        conf.setDaoSuffix("Dao");
+        conf.setSqlManagerFactoryBeanName("sqlManagerFactoryBean");
+        return conf;
+    }
+
+    @Bean(name = "sqlManagerFactoryBean")
+    @Primary
+    public SqlManagerFactoryBean getSqlManagerFactoryBean(@Qualifier("datasource") DataSource datasource) {
+        SqlManagerFactoryBean factory = new SqlManagerFactoryBean();
+
+        BeetlSqlDataSource source = new BeetlSqlDataSource();
+        source.setMasterSource(datasource);;
+        factory.setCs(source);
+        factory.setDbStyle(new MySqlStyle());
+        factory.setInterceptors(new Interceptor[]{new DebugInterceptor()});
+        factory.setNc(new UnderlinedNameConversion());
+        factory.setSqlLoader(new ClasspathLoader("/sql"));
+        return factory;
+    }
+
+
+    @Bean(name="datasource")
+    public DataSource getDataSource() {
+        System.out.println("-------------------- primaryDataSource init ---------------------");
+        return DataSourceBuilder.create().url("jdbc:mysql://127.0.0.1/test").username("root").password("lijzh780214").build();
+    }
+
+
+    @Bean(name="txManager")
+    public DataSourceTransactionManager getDataSourceTransactionManager(@Qualifier("datasource") DataSource datasource) {
+        DataSourceTransactionManager dsm = new DataSourceTransactionManager();
+        dsm.setDataSource(datasource);
+        return dsm;
+    }
+}
--- furniture-web/src/main/java/org/furniture/web/config/BeetlConfig.java
+++ furniture-web/src/main/java/org/furniture/web/config/BeetlConfig.java
@@ -5,30 +5,40 @@ import org.beetl.ext.spring.BeetlGroupUtilConfiguration;
 import org.beetl.ext.spring.BeetlSpringViewResolver;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * @Description ${DESCRIPTION}
+ * @Description BeetlAutoConfiguration
  * @Author bwu
  * @Create 10/06/2018 18:18.
  */
 @Configuration
+@ConditionalOnClass({org.beetl.core.Configuration.class})
+@AutoConfigureAfter({WebMvcAutoConfiguration.class})
 public class BeetlConfig {
-    @Value("${beetl.templatesPath}") String templatesPath;//模板根目录 ，比如 "templates"
+    /**
+     * 模板根目录,比如"templates"
+     */
+    @Value("${spring.beetl.templatesPath}")
+    String templatesPath = "templates";
+
     @Bean(name = "beetlContentTemplateConfig")
     public BeetlGroupUtilConfiguration getBeetlGroupUtilConfiguration() {
         BeetlGroupUtilConfiguration beetlGroupUtilConfiguration = new BeetlGroupUtilConfiguration();
-        //获取Spring Boot 的ClassLoader
+        //获取Spring Boot的ClassLoader
         ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        if(loader==null){
+        if (loader == null) {
             loader = BeetlConfig.class.getClassLoader();
         }
-        //beetlGroupUtilConfiguration.setConfigProperties(extProperties);//额外的配置，可以覆盖默认配置，一般不需要
+        //beetlGroupUtilConfiguration.setConfigProperties(extProperties);//额外的配置,可以覆盖默认配置,一般不需要
         ClasspathResourceLoader cploder = new ClasspathResourceLoader(loader, templatesPath);
         beetlGroupUtilConfiguration.setResourceLoader(cploder);
         beetlGroupUtilConfiguration.init();
-        //如果使用了优化编译器，涉及到字节码操作，需要添加ClassLoader
+        //如果使用了优化编译器,涉及到字节码操作,需要添加ClassLoader
         beetlGroupUtilConfiguration.getGroupTemplate().setClassLoader(loader);
         return beetlGroupUtilConfiguration;
     }
